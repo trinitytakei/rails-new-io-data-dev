d63ee4733e532f80036f16e6f1d1e61937755bf2
Trinity Takei <980204203@railsnew.io>
2025-03-03 10:03:34 +0100

    Update ingredient: Test 1

--- ingredients/Test 1/template.rb
+++ ingredients/Test 1/template.rb
@@ -1,1 +1,5 @@
-Hey ho "Snip 1 content updated" da darihoppa yo
+Hey ho "Snip 1 content updated" da darihoppa yo. Here comes snip2: <<~SNIPPET_2
+Snip 2 content
+it's multi-line
+because why not
+SNIPPET_2
